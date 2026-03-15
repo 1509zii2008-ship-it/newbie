2d9f79782ffb5d26393bc11067832211fa350d87
Danil <pidog@example.com>
2026-03-15 13:46:44 +0200

    Post request commit

--- frontend/newbie/src/Component/ApiRequest/ApiRequest.jsx
+++ frontend/newbie/src/Component/ApiRequest/ApiRequest.jsx
@@ -4,13 +4,23 @@ function ApiRequest(){
 
     async function getUsers(){
         try{
-            const response = await fetch('https://linsey-torporific-peevishly.ngrok-free.dev/users')
+            const response = await fetch('https://newbie-7.onrender.com/users')
             if(!response.ok) throw new Error('Ошибка сети');
             const data = await response.json();
             console.log(data);
         }catch(error){
             console.error(error);
         }
+    }
+
+    async function postUsers(){
+        const response = await fetch('https://newbie-7.onrender.com/users', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({username: 'Danil', password: '1234', email: 'Newbie@gmail.com'})
+        })
+        const data = await response.json();
+        console.log(data)
     }
 
     function handleChange(e){
@@ -20,12 +30,19 @@ function ApiRequest(){
     return(
         <div className="main-block">
             <div className='form-block'>
-                <input className='form' 
+                <input className='email-form' 
+                onChange={handleChange}/>
+                <input className='password-form' 
                 onChange={handleChange}/>
                 <button 
                 className="click-request"
                 onClick={getUsers}
-                >Click
+                >Click Get
+                </button>
+                <button 
+                className="click-request"
+                onClick={postUsers}
+                >Click Post
                 </button>
             </div>
         </div>
